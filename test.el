311b83a07cfa2990566a825644f7eb779f6561ff
Yuta Yamada <cokesboy@gmail.com>
2015-12-26 18:49:46 -0800

    Add a test

---
+++ test.el
@@ -0,0 +1,43 @@
+;;; test.el --- test for this repository -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015 by Yuta Yamada
+
+;; Author: Yuta Yamada <cokesboy"at"gmail.com>
+
+;;; License:
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;; Commentary:
+
+;;; Code:
+
+(require 'ert)
+
+(add-to-list 'load-path
+             (file-name-directory (or #$ (expand-file-name (buffer-file-name)))))
+(require 'libsample)
+
+(ert-deftest mod-test-return-t ()
+  (should (eq t (mod-test-return-t 0)))
+  (should (eq t (mod-test-return-t "abc")))
+  (should (eq t (mod-test-return-t t)))
+  (should (eq t (mod-test-return-t nil)))
+  (should (eq t (mod-test-return-t ?a))))
+
+;; Local Variables:
+;; coding: utf-8
+;; mode: emacs-lisp
+;; no-byte-compile: t
+;; End:
+
+;;; test.el ends here
