4bbf96e4877915a4c76105372aaa647df2f6431e
Yuta Yamada <cokesboy@gmail.com>
2015-12-27 21:56:38 -0800

    Fix wrong type declaration

--- emacs_module.nim
+++ emacs_module.nim
@@ -57,7 +57,7 @@ type
   intmax_t* {.importc: "intmax_t", header: "<inttypes.h>".} = clonglong
   emacs_finalizer_function* {.importc: "emacs_finalizer_function",
                               header: "<emacs-module.h>".} = proc(void: pointer)
-  emacs_value* {.importc: "emacs_value", header: "<emacs-module.h>".} = pointer
+  emacs_value* {.importc: "struct emacs_value_tag", header: "<emacs-module.h>".} = pointer
   ptrdiff_t* {.importc: "ptrdiff_t", header: "<stddef.h>".} = int
   emacs_env* {.importc: "struct emacs_env_25", header: "<emacs-module.h>".} = object
     size: ptrdiff_t
