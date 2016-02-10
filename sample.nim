719e87dd6fe0d9fd2ab68ff9fca9eda39edd5d35
Yuta Yamada <cokesboy@gmail.com>
2016-02-10 11:55:14 -0800

    Just ignore exit code

--- sample.nim
+++ sample.nim
@@ -23,7 +23,7 @@ emacs.addFunc(Fmod_test_return_uname, 1):
   if (env.copy_string_contents(env, args[0], nil, addr len)):
     var buf1 = newString(len)
     if (env.copy_string_contents(env, args[0], addr buf1[0], addr len)):
-      var (res, errC) = execCmdEx("uname " & $buf1 )
+      var (res, _) = execCmdEx("uname " & $buf1 )
       result = env.make_string(env, addr res[0], res.len - 1)
 
 emacs.addFunc(Fmod_test_sum, 1):
