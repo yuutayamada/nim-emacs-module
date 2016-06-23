a29e3c8e106713af1deb2a33cde618f1b8c9e45d
Yuta Yamada <cokesboy@gmail.com>
2016-06-23 16:30:06 -0700

    Use emacs's function name
    and added nimEmacs prefix in nim functions.

--- sample.nim
+++ sample.nim
@@ -9,11 +9,11 @@ import emextra      # helper library
 init(emacs)
 
 # intern
-emacs.defun(Fmod_test_return_t, 1):
+emacs.defun(mod_test_return_t, 1):
   env.intern(env, "t".cstring)
 
 # extract_integer
-emacs.defun(Fmod_test_sum, 2):
+emacs.defun(mod_test_sum, 2):
   assert(nargs == 2)
   let
     a = env.extract_integer(env, args[0])
@@ -23,7 +23,7 @@ emacs.defun(Fmod_test_sum, 2):
   env.make_integer(env, s)
 
 # vec_size
-emacs.defun(Fmod_test_vector_fill, 2):
+emacs.defun(mod_test_vector_fill, 2):
   var
     vec = args[0]
     val = args[1]
@@ -34,7 +34,7 @@ emacs.defun(Fmod_test_vector_fill, 2):
   result = env.intern(env, "t")
 
 # vec_get
-emacs.defun(Fmod_test_vector_eq, 2):
+emacs.defun(mod_test_vector_eq, 2):
   var
     vec = args[0]
     val = args[1]
@@ -45,19 +45,19 @@ emacs.defun(Fmod_test_vector_eq, 2):
   result = env.intern(env, "t")
 
 # non_local_exit_signal
-emacs.defun(Fmod_test_signal, 0):
+emacs.defun(mod_test_signal, 0):
   assert(env.non_local_exit_check(env) == emacs_funcall_exit_return)
   env.non_local_exit_signal(env, env.intern(env, "error"),
                             env.make_integer(env, 100))
 
-emacs.defun(Fmod_test_throw, 0):
+emacs.defun(mod_test_throw, 0):
   assert(env.non_local_exit_check(env) == emacs_funcall_exit_return)
   env.non_local_exit_throw(env, env.intern(env, "tag"),
                            env.make_integer(env, 42))
   result = env.intern(env, "nil")
 
 # copy_string_contents
-emacs.defun(Fmod_test_return_uname_cmd, 1):
+emacs.defun(mod_test_return_uname_cmd, 1):
   var len: ptrdiff_t
   if (env.copy_string_contents(env, args[0], nil, addr len)):
     var buf1 = newString(len)
@@ -66,7 +66,7 @@ emacs.defun(Fmod_test_return_uname_cmd, 1):
       result = env.make_string(env, addr res[0], res.len - 1 )
 
 from osproc import execCmdEx
-emacs.defun(Fmod_test_return_uname, 1):
+emacs.defun(mod_test_return_uname, 1):
   var len: ptrdiff_t
   if (env.copy_string_contents(env, args[0], nil, addr len)):
     var buf1 = newString(len)
