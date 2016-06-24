4419fbae28afb2af95621ffd7797c44a74ba597e
Yuta Yamada <cokesboy@gmail.com>
2016-06-23 22:08:51 -0700

    Move test files into test directory

--- test/test.el
+++ test/test.el
@@ -25,7 +25,8 @@
 
 (add-to-list 'load-path
              (file-name-directory (or #$ (expand-file-name (buffer-file-name)))))
-(require 'libsample)
+
+(require 'sample)
 
 (ert-deftest mod-test-return-t ()
   (should (eq t (mod-test-return-t 0)))
