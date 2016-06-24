864d6d3181203df9a5612497aa9d25a0a4f9da33
Yuta Yamada <cokesboy@gmail.com>
2016-06-24 00:32:14 -0700

    Add comment of license

--- test/sample.nim
+++ test/sample.nim
@@ -1,3 +1,22 @@
+#[
+
+License:
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+]#
+
 # `plugin_is_GPL_compatible` indicates that its code is
 # released under the GPL or compatible license; Emacs will refuse to
 # load modules that don't export such a symbol.
